bba89b2b7599b7ab3f52f553c967bc82ae240541
efulls <saeful.mahmud@gmail.com>
2020-01-24 20:29:30 +0700

    38. Demo: Reference Type and Value Types 2

--- CSharpHelloWorld/Person.cs
+++ CSharpHelloWorld/Person.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿//using System;
 
-namespace CSharpHelloWorld
-{
-    class Person
-    {
-        public string FirstName;
-        public string LastName;
+//namespace CSharpHelloWorld
+//{
+//    class Person
+//    {
+//        public string FirstName;
+//        public string LastName;
 
-        public void Introduce()
-        {
-            Console.WriteLine("My Name is " + FirstName + " " + LastName);
-        }
-    }
-}
+//        public void Introduce()
+//        {
+//            Console.WriteLine("My Name is " + FirstName + " " + LastName);
+//        }
+//    }
+//}
