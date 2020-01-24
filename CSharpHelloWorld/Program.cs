a50ec9f968b65e38463a669d692e6614eaf091fa
postfull <saeful.mahmud@gmail.com>
2020-01-24 09:19:59 +0700

    18. Type Data

--- CSharpHelloWorld/Program.cs
+++ CSharpHelloWorld/Program.cs
@@ -6,7 +6,19 @@ namespace CSharpHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Dunia");
+            byte number         = 255;
+            int count           = 20;
+            float totalPrice    = 22.19f;
+            char character      = 'A';
+            string firstName    = "Saeful";
+            bool isWorking      = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
