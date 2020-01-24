28216789429116bf16b8a32f491bf62b9d722963
postfull <saeful.mahmud@gmail.com>
2020-01-24 08:48:36 +0700

    First Commit Console.WriteLine()

--- CSharpHelloWorld/Program.cs
+++ CSharpHelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpHelloWorld
 {
@@ -10,6 +6,7 @@ namespace CSharpHelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hallo Dunia");
         }
     }
 }
