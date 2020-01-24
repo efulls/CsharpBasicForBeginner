51d72f9884b2d22b8ad6b9dc046f547691e956b9
postfull <saeful.mahmud@gmail.com>
2020-01-24 09:20:21 +0700

    18.MinValue & Max Value

--- CSharpHelloWorld/Program.cs
+++ CSharpHelloWorld/Program.cs
@@ -6,19 +6,8 @@ namespace CSharpHelloWorld
     {
         static void Main(string[] args)
         {
-            byte number         = 255;
-            int count           = 20;
-            float totalPrice    = 22.19f;
-            char character      = 'A';
-            string firstName    = "Saeful";
-            bool isWorking      = false;
-
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
         }
     }
 }
