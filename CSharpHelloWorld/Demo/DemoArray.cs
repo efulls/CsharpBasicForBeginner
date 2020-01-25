e735165366d2ac43f3d02175b0a10340fc386228
efulls <saeful.mahmud@gmail.com>
2020-01-25 13:24:43 +0700

    Memisah Program.cs dengan Demo.cs

---
+++ CSharpHelloWorld/Demo/DemoArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHelloWorld.Demo
+{
+    class DemoArray
+    {
+        public void ArraySample()
+        {
+            var numbers = new[] { 1, 2, 5, 4, 20, 15, 33 };
+
+            //length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            //Clear from array 1 - 3
+            Array.Clear(numbers, 1, 3);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            //Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+
+            Console.WriteLine("Efect Of Copy()");
+            foreach (var n in another)
+                Console.WriteLine(n);
+
+            //Short()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Efect od sort()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+
+            //Reverse()
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Referse()");
+            foreach (var n in numbers)
+                Console.WriteLine(n);
+        }
+    }
+}
--- CSharpHelloWorld/Program.cs
+++ CSharpHelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpHelloWorld.ControlFlow;
+using CSharpHelloWorld.Demo;
 using System;
 
 namespace CSharpHelloWorld
@@ -7,39 +8,17 @@ namespace CSharpHelloWorld
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 1, 2, 5, 4, 20, 15, 33 };
 
-            //length
-            Console.WriteLine("Length: " + numbers.Length);
+            /*
+            Ex:
+            var myvar = new MyClass();
+            myvar.MyMethod();
 
-            //Clear from array 1 - 3
-            Array.Clear(numbers, 1, 3);
+            var Darray = new DemoArray();
+            Darray.ArraySample();
+             */
 
-            Console.WriteLine("Effect of Clear()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
 
-            //Copy()
-            int[] another = new int[3];
-            Array.Copy(numbers, another, 3);
-
-            Console.WriteLine("Efect Of Copy()");
-            foreach (var n in another)
-                Console.WriteLine(n);
-
-            //Short()
-            Array.Sort(numbers);
-
-            Console.WriteLine("Efect od sort()");
-            foreach (var n in numbers)
-                Console.WriteLine(n);
-
-            //Reverse()
-            Array.Reverse(numbers);
-
-            Console.WriteLine("Effect of Referse()");
-            foreach(var n in numbers)
-                Console.WriteLine(n);
         }
 
     }
