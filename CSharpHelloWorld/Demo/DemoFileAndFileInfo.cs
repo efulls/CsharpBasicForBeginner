288cd2be99ad5b0cae44ecb51d1c10e9191073e6
efulls <saeful.mahmud@gmail.com>
2020-01-25 21:38:04 +0700

    73. Demo File & File Info

---
+++ CSharpHelloWorld/Demo/DemoFileAndFileInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHelloWorld.Demo
+{
+    class DemoFileAndFileInfo
+    {
+        public void SampleFile()
+        {
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg");
+            File.Delete(path);
+            if(File.Exists(path))
+            {
+                //TODO:
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if(fileInfo.Exists)
+            {
+                //TODO:
+            }
+        }
+    }
+}
